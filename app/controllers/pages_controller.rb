b8cda86c4079a5f723f28c69e9a4fc4efb2bfb24
Vitaliy Gambala <gambala.rus@gmail.com>
2015-10-15 17:39:35 +0300

    Uodate gems and Remove pages index route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,6 @@ class PagesController < ApplicationController
   before_filter :authenticate_admin_user!, only: :admin
   layout 'admin', only: :admin
 
-  def index
-  end
-
   def show
     @main_menu_key = "page_#{params[:id]}"
     @page = params[:id]
