077439ba2291396c3d622b4d3b66fe47e07c7222
Vitaliy Gambala <gambala.rus@gmail.com>
2015-11-11 05:19:32 +0300

    Fix N+1 issue

--- app/controllers/admin/call_requests_controller.rb
+++ app/controllers/admin/call_requests_controller.rb
@@ -2,7 +2,7 @@ module Admin
   class CallRequestsController < AdminController
     def index
       @main_menu_key = :call_requests
-      @call_requests = CallRequest.all.order(created_at: :desc)
+      @call_requests = CallRequest.all.includes(:ordering_profile, :receiving_profile).order(created_at: :desc)
       # render :index if stale? @call_requests | layout_resources
     end
   end
