be7fd2caceee067c193c63f8a7542788c0fe16c5
Vitaliy Gambala <gambala.rus@gmail.com>
2015-11-11 04:40:43 +0300

    Refactor useful methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def pages
+    @pages ||= Page.all.includes(:viewable_resource).order(:slug)
+  end
+
   def forbidden
     redirect_to root_path, status: :forbidden
   end
@@ -21,13 +25,9 @@ class ApplicationController < ActionController::Base
     forbidden unless current_user.role.try(:admin?)
   end
 
-  def pages
-    @pages ||= Page.all.includes(:viewable_resource).order(:slug)
+  def current_profile
+    @current_profile ||= profile_from_session || profile_for_user || profile_for_guest
   end
-
-  # def layout_resources
-  #   [current_user_etag, pages, revision]
-  # end
 
   def profile_from_session
     Profile.find_by(id: session[:profile_id]) if session[:profile_id]
@@ -44,19 +44,19 @@ class ApplicationController < ActionController::Base
     Profile.new
   end
 
+  # def layout_resources
+  #   [current_user_etag, pages, revision]
+  # end
+
   # def current_order
   #   @current_order ||= current_profile.ordering_orders.with_status(:created).first_or_initialize
   # end
 
-  def current_profile
-    @current_profile ||= profile_from_session || profile_for_user || profile_for_guest
-  end
+  # def current_user_etag
+  #   current_user || :guest
+  # end
 
-  def current_user_etag
-    current_user || :guest
-  end
-
-  def revision
-    @revision ||= { last_modified: File.atime("#{Rails.root}/REVISION").utc, etag: :revision }
-  end
+  # def revision
+  #   @revision ||= { last_modified: File.atime("#{Rails.root}/REVISION").utc, etag: :revision }
+  # end
 end
