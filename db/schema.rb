389a592c191e30250e2715ea2d00ce27ddf81ad2
Vitaliy Gambala <gambala.rus@gmail.com>
2016-01-10 15:07:47 +0300

    Add index to authentications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227233215) do
+ActiveRecord::Schema.define(version: 20160110120559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20151227233215) do
   end
 
   add_index "authentications", ["provider", "uid"], name: "index_authentications_on_provider_and_uid", using: :btree
+  add_index "authentications", ["user_id"], name: "index_authentications_on_user_id", using: :btree
 
   create_table "call_requests", force: :cascade do |t|
     t.integer  "ordering_profile_id"
