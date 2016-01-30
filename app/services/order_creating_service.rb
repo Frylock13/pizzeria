83b37a2eed2a137928a0dc7073a5b53d4baeecfd
Vitaliy Gambala <gambala.rus@gmail.com>
2016-01-30 13:06:19 +0300

    Fix nil record bugs from rollbar

--- app/services/order_creating_service.rb
+++ app/services/order_creating_service.rb
@@ -12,7 +12,7 @@ class OrderCreatingService
     deliver_email = true if user.new_record?
     user.bonus_points += (order.price * 0.1).floor if order.payment_method == :cash
     user.bonus_points -= order.price if order.payment_method == :bonus_points
-    user.save
+    user.save!
     user.deliver_reset_password_instructions! if deliver_email == true
     SmsWorker.perform_async(ENV['SMSAERO_PHONE'], 'Оформлен заказ на vpzven.ru')
   end
--- app/services/pizza_recalculating_service.rb
+++ app/services/pizza_recalculating_service.rb
@@ -16,17 +16,13 @@ class PizzaRecalculatingService
                         .map { |item| item.price(pizza_size) }.sum
         pizza.pizza_attributes.detect { |r| r.pizza_size == pizza_size }
              .assign_attributes(price: pizza.price(pizza_size, fair: true) + dprice)
+        pizza.pizza_attributes.detect { |r| r.pizza_size == pizza_size }
+             .assign_attributes(weight: pizza.weight(pizza_size, fair: true))
       end
     else
       PizzaSizes.pizza_size.values.each do |pizza_size|
         pizza.pizza_attributes.detect { |r| r.pizza_size == pizza_size }
              .assign_attributes(price: pizza.price(pizza_size, fair: true))
-      end
-    end
-    PizzaSizes.pizza_size.values.each do |pizza_size|
-      if parent.weight(pizza_size).present?
-        pizza.pizza_attributes.detect { |r| r.pizza_size == pizza_size }
-             .assign_attributes(weight: pizza.weight(pizza_size, fair: true))
       end
     end
   end
