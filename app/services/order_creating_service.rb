33b9d17d81b3575cfd82b12c6e252a1b4b9161a8
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-04-04 16:40:46 +0300

    Autologin user after ordering

--- app/services/order_creating_service.rb
+++ app/services/order_creating_service.rb
@@ -1,5 +1,6 @@
 class OrderCreatingService
   attr_accessor :order
+  attr_accessor :user
 
   def initialize(order)
     @order = order
@@ -7,13 +8,12 @@ class OrderCreatingService
 
   def submit
     order.update(booked_on: Time.zone.now) unless order.booked_on?
-    user = User.where(email: order.ordering_profile.email)
-               .first_or_initialize(password: SecureRandom.hex(16))
-    deliver_email = true if user.new_record?
+    @user = User.where(email: order.ordering_profile.email)
+                .first_or_initialize(password: SecureRandom.hex(16))
     user.bonus_points += (order.price * 0.1).floor if order.payment_method == :cash
     user.bonus_points -= order.price if order.payment_method == :bonus_points
     user.save!
-    user.deliver_reset_password_instructions! if deliver_email == true
+    user.deliver_reset_password_instructions! if user.new_record?
     SmsWorker.perform_async(ENV['SMSAERO_PHONE'], 'Оформлен заказ на vpzven.ru')
   end
 end
