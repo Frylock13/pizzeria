1b815e71263cd420053071308a15b86c9860bed0
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-04-12 17:29:42 +0300

    Add smsc.ru service

---
+++ app/services/sms_c_service.rb
@@ -0,0 +1,24 @@
+require 'digest/md5'
+class SmsCService
+  attr_accessor :phone, :text
+
+  def initialize(phone, text)
+    @phone = phone
+    @text = text
+  end
+
+  def send
+    url = "#{ENV['SMSC_LINK']}?#{URI.encode_www_form(hash)}"
+    URI.parse(url).read
+  end
+
+  def hash
+    {
+      login: ENV['SMSC_LOGIN'],
+      psw: Digest::MD5.hexdigest(ENV['SMSC_PASSWORD']),
+      phones: phone,
+      mes: URI.encode(text)
+      sender: ENV['SMSC_FROM']
+    }
+  end
+end
--- app/workers/sms_worker.rb
+++ app/workers/sms_worker.rb
@@ -3,6 +3,6 @@ class SmsWorker
 
   def perform(phone, text)
     return logger.debug { text } if Rails.env.development?
-    SmsService.new(phone, text).send
+    SmsCService.new(phone, text).send
   end
 end
