166f253eda1098da8eac7910d1b0817996b3096c
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-05-19 09:57:54 +0300

    Add pages for adding pizzas and products to order

--- app/controllers/web/admin/orders/application_controller.rb
+++ app/controllers/web/admin/orders/application_controller.rb
@@ -1,7 +1,14 @@
 class Web::Admin::Orders::ApplicationController < Web::Admin::ApplicationController
+  before_action :menu_key
+  helper_method :order
+
   private
 
   def order
     @order ||= Order.find(params[:order_id])
+  end
+
+  def menu_key
+    @menu_key = :orders
   end
 end
