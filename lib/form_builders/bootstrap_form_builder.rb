5ef96bb718045096d5e28f6ed47da77eaaf4c64c
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-07-23 08:59:35 +0300

    Remove old form helper from new BootstrapFormBuilder

--- lib/form_builders/bootstrap_form_builder.rb
+++ lib/form_builders/bootstrap_form_builder.rb
@@ -12,11 +12,3 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
     @template.content_tag(:div, options) { yield }
   end
 end
-
-# module FormsHelper
-#   def form_field_group(field_name, form_record, div_class: nil, div_id: nil, &block)
-#     classes = ['form-group', div_class]
-#     classes << 'has-error' if form_record.errors[field_name].any?
-#     content_tag :div, capture(&block), class: classes.join(' '), id: div_id
-#   end
-# end
