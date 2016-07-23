e01341f7405ba9f15f1cf8d84f5728061d552a13
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-07-23 09:49:46 +0300

    Remove old menu_link_to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
-  def old_menu_link_to(menu_key, *args, &block)
-    content_tag :li, link_to(*args, &block), class: "#{'active' if menu_key == @menu_key}"
-  end
-
   def tab_link(text, path, active:, disabled: nil)
     content_tag :li, class: "#{'active' if active} #{'disabled' if disabled == true}" do
       link_to path do
